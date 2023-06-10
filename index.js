e6bf5acfb4392bcb46dac874149f05b1967bb5e1
MD Tariqul Islam <ittarek551@gmail.com>
2023-06-11 01:44:41 +0600

    admin verify

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const app = express();
 const cors = require("cors");
 const jwt = require("jsonwebtoken");
 require("dotenv").config();
-const stripe = require("stripe")(process.env.PAYMENT_SECRET_KEY);
+// const stripe = require("stripe")(process.env.PAYMENT_SECRET_KEY);
 const port = process.env.PORT || 5000;
 
 // middleware
@@ -51,15 +51,15 @@ async function run() {
     await client.connect();
 
     const classCollection = client.db("summerClass").collection("classes");
-    const userCollection = client.db("summerClass").collection("users");
+    const usersCollection = client.db("summerClass").collection("users");
 
     // jwt api
     app.post("/jwt", (req, res) => {
-      const user = req.body;
-      const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, {
+      const users = req.body;
+      const token = jwt.sign(users, process.env.ACCESS_TOKEN_SECRET, {
         expiresIn: "1h",
       });
-      console.log(token);
+      // console.log("token", token);
       res.send({ token });
     });
 
@@ -67,11 +67,11 @@ async function run() {
     const verifyAdmin = async (req, res, next) => {
       const email = req.decoded.email;
       const query = { email: email };
-      const user = await userCollection.findOne(query);
+      const user = await usersCollection.findOne(query);
       if (user?.roll !== "admin") {
         return res
           .status(403)
-          .send({ error: true, message: "forbidden access" });
+          .send({ error: true, message: "forbidden access you are no admin" });
       }
       next();
     };
@@ -79,7 +79,7 @@ async function run() {
     const verifyInstructors = async (req, res, next) => {
       const email = req.decoded.email;
       const query = { email: email };
-      const user = await userCollection.findOne(query);
+      const user = await usersCollection.findOne(query);
       if (user?.roll !== "instructor") {
         return res
           .status(403)
@@ -91,7 +91,7 @@ async function run() {
     const studentVerify = async (req, res, next) => {
       const email = req.decoded.email;
       const query = { email: email };
-      const user = await userCollection.findOne(query);
+      const user = await usersCollection.findOne(query);
       if (user?.roll !== "student") {
         return res
           .status(403)
@@ -101,8 +101,8 @@ async function run() {
     };
 
     // user get api
-    app.get("/users", verifyJwt, verifyAdmin, async (req, res) => {
-      const result = await userCollection.find().toArray();
+    app.get("/users", async (req, res) => {
+      const result = await usersCollection.find().toArray();
       res.send(result);
     });
 
@@ -110,24 +110,27 @@ async function run() {
     app.post("/users", async (req, res) => {
       const user = req.body;
       const query = { email: user.email };
-      const existingUser = await userCollection.findOne(query);
+      const existingUser = await usersCollection.findOne(query);
       if (existingUser) {
         return res.send({ message: "user already exist" });
       }
-      const result = await userCollection.insertOne(user);
+      const result = await usersCollection.insertOne(user);
       res.send(result);
     });
 
     // check admin by email using get method
-    app.get("/users/admin/:email", verifyJwt, async (req, res) => {
+    app.get("/users/admin/:email", verifyJwt, verifyAdmin, async (req, res) => {
       const email = req.params.email;
 
-      if (req.decoded.email !== email) {
-        res.send({ admin: false });
-      }
+      // if (req.decoded.email !== email) {
+      //   res.send({ admin: false });
+      // }
+
       const query = { email: email };
-      const user = await userCollection.findOne(query);
+      // console.log("130  admin query", query);
+      const user = await usersCollection.findOne(query);
       const result = { admin: user?.roll === "admin" };
+      // console.log("132 admin result", result);
       res.send(result);
     });
 
@@ -140,7 +143,61 @@ async function run() {
           roll: "admin",
         },
       };
-      const result = await userCollection.updateOne(filter, updateDoc);
+      const result = await usersCollection.updateOne(filter, updateDoc);
+      res.send(result);
+    });
+    // make instructor api
+    app.patch("/users/instructor/:id", async (req, res) => {
+      const id = req.params.id;
+      const filter = { _id: new ObjectId(id) };
+      const updateDoc = {
+        $set: {
+          roll: "instructor",
+        },
+      };
+      const result = await usersCollection.updateOne(filter, updateDoc);
+      res.send(result);
+    });
+    // instructors get api
+    app.get("/users/instructors/:email", async (req, res) => {
+      const email = req.params.email;
+      // console.log(email);
+      if (req.decoded.email !== email) {
+        res.send({ admin: false });
+      }
+
+      const query = { email: email };
+      const user = await usersCollection.findOne(query);
+      const result = { instructor: user?.roll === "instructor" };
+
+      res.send(result);
+    });
+
+    // make student api
+    // app.patch("/users/student/:id", async (req, res) => {
+    //   const id = req.params.id;
+    //   const filter = { _id: new ObjectId(id) };
+    //   const updateDoc = {
+    //     $set: {
+    //       roll: "student",
+    //     },
+    //   };
+    //   const result = await usersCollection.updateOne(filter, updateDoc);
+    //   res.send(result);
+    // });
+
+    // student get api
+    app.get("/users/student/:email", async (req, res) => {
+      const email = req.params.email;
+      // console.log(email);
+      if (req.decoded.email !== email) {
+        res.send({ admin: false });
+      }
+
+      const query = { email: email };
+      const user = await usersCollection.findOne(query);
+      const result = { student: user?.roll === "student" };
+
       res.send(result);
     });
 
@@ -172,7 +229,7 @@ async function run() {
     });
 
     // Send a ping to confirm a successful connection
-    await client.db("admin").command({ ping: 1 });
+    // await client.db("admin").command({ ping: 1 });
     console.log(
       "Pinged your deployment. You successfully connected to MongoDB!"
     );
